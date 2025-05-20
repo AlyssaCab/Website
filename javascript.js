ff2f56a9074ddfa4957d535b560624cfede14668
AlyssaCab <143555772+AlyssaCab@users.noreply.github.com>
2025-05-20 19:02:06 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
+function toggleMenu() {
+  const menu = document.querySelector('.tab-bar');
+  const hamburger = document.querySelector('.hamburger');
+  const icon = hamburger.querySelector('.icon');
+
+  menu.classList.toggle('show');
+
+  if (menu.classList.contains('show')) {
+    icon.textContent = '×'; // X symbol
+    icon.classList.add('close-icon'); // Add class to enlarge
+  } else {
+    icon.textContent = '☰'; // Hamburger symbol
+    icon.classList.remove('close-icon'); // Remove enlarged class
+  }
+}
+
 function openPopup() {
   document.getElementById("emailPopup").style.display = "block";
 }
@@ -12,4 +28,3 @@ window.onclick = function(event) {
     closePopup();
   }
 };
-
